8f83725e99a217d101e45519cc08c9e1736bd6ab
altug engin <altugengin@yahoo.com>
2025-10-24 22:41:50 +0300

    fetching table

--- src/payments/page.tsx
+++ src/payments/page.tsx
@@ -1,25 +1,38 @@
 import { supabase } from "@/supabaseClient";
 import { columns, type Payment } from "./columns";
-import { useState } from "react";
+import { Spinner } from "@/components/ui/spinner";
+import { Item, ItemContent, ItemMedia, ItemTitle } from "@/components/ui/item";
 
 import { DataTable } from "./data-table";
 import { useQuery } from "@tanstack/react-query";
 
-const fetchPosts=async():Promise<Payment[]> =>{
-  const {data}=await supabase.from("payment").select("*")
-  return data as Payment[]
-}
+const fetchPosts = async (): Promise<Payment[]> => {
+  const { data } = await supabase.from("payment").select("*");
+  return data as Payment[];
+};
 
 export default function Demo() {
- const {data}=useQuery({queryKey:["payment"],queryFn:fetchPosts})
- console.log(typeof data)
- console.log(data[1].amount)
+  const { data, isLoading } = useQuery({
+    queryKey: ["payment"],
+    queryFn: fetchPosts,
+  });
 
-
+  //const [datak, setDatak] = useState(data);
+  console.log(data);
   return (
     <div className="container mx-auto py-10">
-     {/*<DataTable columns={columns} data={data}></DataTable>*/}
-     <DataTable columns={columns} data={data}></DataTable>
+      {isLoading ? (
+        <Item>
+          <ItemMedia>
+            <Spinner></Spinner>
+          </ItemMedia>
+          <ItemContent>
+            <ItemTitle>Tablo Yükleniyor...</ItemTitle>
+          </ItemContent>
+        </Item>
+      ) : (
+        <DataTable columns={columns} data={data!}></DataTable>
+      )}
     </div>
   );
 }
