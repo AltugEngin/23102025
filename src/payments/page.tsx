83ec5de2286cbe8a4e3c08a5f2d228d5ae1aadde
altug engin <altugengin@yahoo.com>
2025-10-28 15:06:02 +0300

    rhf added

--- src/payments/page.tsx
+++ src/payments/page.tsx
@@ -1,10 +1,13 @@
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
+import { RecordAddForm } from "./form";
 
 export default function Demo() {
   return (
     <div className="container mx-auto py-10">
       <DataTable columns={columns}></DataTable>
+      {/* <RecordAddForm></RecordAddForm> */}
+      <RecordAddForm></RecordAddForm>
     </div>
   );
 }
